36310e8a91f235436420e793edf0aa29cbb6cb55
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2016-09-15 17:28:48 -0300

    Updated CustomerController

--- src/main/java/com/electricalweb/controllers/CustomerController.java
+++ src/main/java/com/electricalweb/controllers/CustomerController.java
@@ -23,7 +23,7 @@ public class CustomerController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
     }
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) {
